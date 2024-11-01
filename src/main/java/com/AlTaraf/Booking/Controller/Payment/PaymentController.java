7434fa0dfd4d016cd1b078a84fcda147a8dfd66a
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-11-01 16:16:00 +0200

    PaymentController renaming

--- src/main/java/com/AlTaraf/Booking/Controller/Payment/PaymentController.java
+++ src/main/java/com/AlTaraf/Booking/Controller/Payment/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
-public class Paymentcontroller {
+public class PaymentController {
 
     @Autowired
     PaymentService paymentService;
