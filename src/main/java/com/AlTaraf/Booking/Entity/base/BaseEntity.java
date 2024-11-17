8ebf7f5ca1158025f1634944a134edf4e56ce63d
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-11-17 10:17:32 +0200

    BaseEntity & BaseAuditEntity

---
+++ src/main/java/com/AlTaraf/Booking/Entity/base/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.AlTaraf.Booking.Entity.base;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity<ID extends Serializable> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
