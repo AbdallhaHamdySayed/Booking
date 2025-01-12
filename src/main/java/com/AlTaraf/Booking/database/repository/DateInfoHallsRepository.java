43e2a971c0c0569556a381ffc7d92e29360d9d7b
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-12 07:34:26 +0200

    DateInfoHalls findByDate

--- src/main/java/com/AlTaraf/Booking/database/repository/DateInfoHallsRepository.java
+++ src/main/java/com/AlTaraf/Booking/database/repository/DateInfoHallsRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface DateInfoHallsRepository extends JpaRepository<DateInfoHalls, Long> {
     @Query("SELECT d FROM DateInfoHalls d WHERE d.reserveDateHalls.id = :reserveDateHallsId")
     DateInfoHalls findByreserveDateHallsId(@Param("reserveDateHallsId") Long reserveDateHallsId);
+
+    @Query("SELECT d FROM DateInfoHalls d WHERE d.date = :date")
+    List<DateInfoHalls> findByDate(@Param("date") LocalDate date);
 }
