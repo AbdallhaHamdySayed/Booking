14186e806ef8be24699ad450f87844cdee70e9f7
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-11 21:44:28 +0200

    ApplicationConfig Refactor

--- src/main/java/com/AlTaraf/Booking/config/ApplicationConfig.java
+++ src/main/java/com/AlTaraf/Booking/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.AlTaraf.Booking.config;
 
+import com.AlTaraf.Booking.database.repository.UserRepository;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -7,6 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,12 +29,39 @@ import java.util.TimeZone;
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     @PostConstruct
     public void init() {
         // Setting Spring Boot SetTimeZone
         TimeZone.setDefault(TimeZone.getTimeZone("Africa/Tripoli"));
         System.out.println("Spring boot application running in UTC+2 timezone: " + TimeZone.getDefault().getID());
     }
+
+//    @Bean
+//    public AuditorAware<Integer> auditorAware() {
+//        return new ApplicationAuditAware();
+//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
