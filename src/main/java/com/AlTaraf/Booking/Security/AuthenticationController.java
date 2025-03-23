17a79742bf76ff1fb4d61dd044bfd9c6c130cbc1
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-23 16:03:54 +0200

    test login info

--- src/main/java/com/AlTaraf/Booking/Security/AuthenticationController.java
+++ src/main/java/com/AlTaraf/Booking/Security/AuthenticationController.java
@@ -2,6 +2,9 @@ package com.AlTaraf.Booking.Security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -19,5 +22,12 @@ public class AuthenticationController {
   public ResponseEntity<AuthenticationResponse> authenticateMobile(@RequestBody AuthenticationRequest request) {
     return ResponseEntity.ok(handler.mobileAuthenticate(request));
   }
+
+//  @GetMapping(value = "/login-info")
+//  @PreAuthorize("isAuthenticated()")
+//  public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+//    return handler.getUserInfo(userDetails);
+//  }
+
 
 }
