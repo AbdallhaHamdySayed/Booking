27e35f3154421bf6957d72502782902507aca10f
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-24 15:13:51 +0200

    Response Keys

---
+++ src/main/java/com/AlTaraf/Booking/response/keys/ErrorStatus.java
@@ -0,0 +1,57 @@
+package com.AlTaraf.Booking.response.keys;
+
+public enum ErrorStatus {
+
+    SYSTEM_HAVE_ISSUE("SYSTEM_HAVE_ISSUE", "يوجد مشكله فى النظام يرجى مراجعه الادمن", "System Error Call System Admin  ", "", ""),
+    INVALID_JWT_SIGNATURE("INVALID_JWT_SIGNATURE", " مشكله فى ال JWT", "invalid jwt signature", "", ""),
+    INVALID_JWT_TOKEN("INVALID_JWT_TOKEN", " مشكله فى ال JWT", "INVALID JWT TOKEN", "", ""),
+    EXPIRED_JWT_EXCEPTION("EXPIRED_JWT_EXCEPTION", " تم انتهاء الصلاحيه يرجى تسجيل الدخول", "expired jwt exception", "", ""),
+    JWT_EXCEPTION("JWT_EXCEPTION", "الصلاحيه غير فعاله", "jwt exception", "", ""),
+    ACCOUNT_LOCKED("ACCOUNT_LOCKED", "اﻻكونت غير فعال / معلق", "Account Is Locked", "", ""),
+    ACCOUNT_NOT_FOUND("ACCOUNT_NOT_FOUND", "بيانات الدخول غير صحيحه", "Login Data Is incorrect..", "", ""),
+    USER_EMAIL_TOKEN("USER_EMAIL_TOKEN", "اليوزر نيم مستخدم من قبل ", "User Name Already token ..", "", ""),
+    ACCOUNT_MUST_CHANGE_PASSWORD_TO_ENABLE_REFRESH_TOKEN("ACCOUNT_MUST_CHANGE_PASSWORD_TO_ENABLE_REFRESH_TOKEN", "يجب اجراء عمليه تغير الباسورد ليتم استكمال العمليه  ", "User Must Change Password Yto Enable New Refresh Token", "", ""),
+    HAVE_RELATED_DATA("HAVE_RELATED_DATA", "يوجد بيانات اخرى متعلقه ", "Delete Not Allowed ", "لايمكن الحذف لوجود بيانات متعلقه بالسجل", "Have Child Data Delete Not Allowed  "),
+    HAVE_DUPLICATE_DATA("HAVE_DUPLICATE_DATA", "يوجد بيانات مكرره سيتم التراجع عن العمليه ", "  ", "يوجد بيانات مكرره سيتم التراجع عن العمليه ", "Have Child Data Delete Not Allowed  "),
+    NOT_ALLOWED_TO_WITHDROW("NOT_ALLOWED_TO_WITHDROW", " المبلغ المطلوب غير مسموح", "not allowed to withdraw", "", ""),
+    CURRENT_BALANCE_IS_INSUFFICIENT("CURRENT_BALANCE_IS_INSUFFICIENT", "الرصيد الحالى غير كافى برجاء شحن االرصيد", "please recharge your balance", "", ""),
+    NOT_ALLOWED_TO_COMPLETE_BECAUSE_OF_EXCEPTION("NOT_ALLOWED_TO_COMPLETE_BECAUSE_OF_EXCEPTION", "يوجد خطأ برمجى", "found error Exception", "", ""),
+    UNABLE_TO_DELETE_CUSTOMER("UNABLE_TO_DELETE_CUSTOMER","لا يمكن حذف العميل","Unable to delete the customer","",""),
+    DATA_ALREADY_EXISTS("DATA_ALREADY_EXISTS", "بعض البيانات المقدمة موجودة بالفعل", "Some of the provided data already exist", "", ""),
+    INVALID_OTP("INVALID_OTP", "رمز الدخول غير صحيح", "Invalid otp", "", ""),
+    NOT_ACCEPTABLE_PROFIT_AMOUNT("NOT_ACCEPTABLE_PROFIT_AMOUNT",   "القيمه غير مقبوله ", "There are no profits for this client", "", ""  ),
+    VALUE_GREATER_THAN_PROFIT("VALUE_GREATER_THAN_PROFIT", "لاتوجد تلك القيمة في محفظة الأرباح", "This value is not in the profit wallet", "", "");
+    private final String errorKey;
+    private final String arabicMessage;
+    private final String englishMessage;
+    private final String fixAr;
+    private final String fixEn;
+
+    ErrorStatus(String errorKey, String arabicMessage, String englishMessage, String fixAr, String fixEn) {
+        this.errorKey = errorKey;
+        this.arabicMessage = arabicMessage;
+        this.englishMessage = englishMessage;
+        this.fixAr = fixAr;
+        this.fixEn = fixEn;
+    }
+
+    public String getErrorKey() {
+        return errorKey;
+    }
+
+    public String getArabicMessage() {
+        return arabicMessage;
+    }
+
+    public String getEnglishMessage() {
+        return englishMessage;
+    }
+
+    public String getFixAr() {
+        return fixAr;
+    }
+
+    public String getFixEn() {
+        return fixEn;
+    }
+}
---
+++ src/main/java/com/AlTaraf/Booking/response/keys/ResponseKeys.java
@@ -0,0 +1,33 @@
+package com.AlTaraf.Booking.response.keys;
+
+public class ResponseKeys {
+
+    // generic Exception Error
+    public static final String SYSTEM_HAVE_ISSUE = "SYSTEM_HAVE_ISSUE";
+
+    // Security  Exception Error
+    public static final String INVALID_JWT_SIGNATURE = "INVALID_JWT_SIGNATURE";
+    public static final String INVALID_JWT_TOKEN = "INVALID_JWT_TOKEN";
+    public static final String EXPIRED_JWT_EXCEPTION = "EXPIRED_JWT_EXCEPTION";
+    public static final String JWT_EXCEPTION = "JWT_EXCEPTION";
+    public static final String ACCOUNT_LOCKED = "ACCOUNT_LOCKED";
+    public static final String ACCOUNT_NOT_FOUND = "ACCOUNT_NOT_FOUND";
+    public static final String USER_EMAIL_TOKEN = "USER_EMAIL_TOKEN";
+    public static final String ACCOUNT_MUST_CHANGE_PASSWORD_TO_ENABLE_REFRESH_TOKEN = "ACCOUNT_MUST_CHANGE_PASSWORD_TO_ENABLE_REFRESH_TOKEN";
+
+    public static final String HAVE_RELATED_DATA = "HAVE_RELATED_DATA";
+    public static final String HAVE_DUPLICATE_DATA = "HAVE_DUPLICATE_DATA";
+    public static final String NOT_ALLOWED_TO_WITHDROW = "NOT_ALLOWED_TO_WITHDROW";
+    public static final String CURRENT_BALANCE_IS_INSUFFICIENT = "CURRENT_BALANCE_IS_INSUFFICIENT";
+    public static final String NOT_ALLOWED_TO_COMPLETE_BECAUSE_OF_EXCEPTION = "NOT_ALLOWED_TO_COMPLETE_BECAUSE_OF_EXCEPTION";
+
+    // Company ERROR 2
+    public static final String UNABLE_TO_DELETE_CUSTOMER = "UNABLE_TO_DELETE_CUSTOMER";
+    public static final String DATA_ALREADY_EXISTS = "DATA_ALREADY_EXISTS";
+    public static final String INVALID_OTP = "INVALID_OTP";
+
+    public static final String NOT_ACCEPTABLE_PROFIT_AMOUNT = "NOT_ACCEPTABLE_PROFIT_AMOUNT";
+
+    public static final String VALUE_GREATER_THAN_PROFIT = "VALUE_GREATER_THAN_PROFIT";
+
+}
